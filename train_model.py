3c92cb81144084cbde967e3db11ebe3ef64c5578
hornsa <hornsa@vcu.edu>
2025-04-09 20:08:42 -0400

    added more training data

--- train_model.py
+++ train_model.py
@@ -13,9 +13,16 @@ drug_pairs = [
     'levothyroxine calcium', 'ciprofloxacin antacids', 'methotrexate NSAIDs', 'tramadol SSRIs',
     'phenytoin folic acid', 'fluoxetine tramadol', 'doxycycline dairy', 'nitroglycerin sildenafil',
     'spironolactone potassium', 'clonidine beta-blockers', 'acetaminophen hydrocodone',
-    'amoxicillin clavulanate', 'aspirin acetaminophen', 'loratadine pseudoephedrine'
+    'amoxicillin clavulanate', 'aspirin acetaminophen', 'loratadine pseudoephedrine',
+    'ibuprofen aspirin', 'paracetamol caffeine', 'metformin aspirin', 'atorvastatin amlodipine',
+    'omeprazole clopidogrel', 'warfarin ibuprofen', 'aspirin clopidogrel', 'metoprolol amlodipine',
+    'tramadol acetaminophen', 'prednisone ibuprofen', 'amoxicillin azithromycin', 'lisinopril aspirin',
+    'atorvastatin warfarin', 'metformin glipizide', 'hydrochlorothiazide lisinopril',
+    'gabapentin hydrocodone', 'oxycodone acetaminophen', 'naproxen ibuprofen', 'aspirin naproxen',
+    'ibuprofen diclofenac', 'paracetamol ibuprofen', 'amoxicillin doxycycline', 'ciprofloxacin metronidazole',
+    'levothyroxine iron', 'fluoxetine sertraline', 'diazepam lorazepam', 'clonazepam alprazolam',
+    'sildenafil tadalafil', 'metformin sitagliptin', 'atorvastatin ezetimibe'
 ]
-
 interaction_labels = [
     1, 1, 0, 0,
     1, 1, 0, 0,
@@ -24,6 +31,14 @@ interaction_labels = [
     1, 1, 1, 1,
     1, 1, 1, 1,
     1, 0, 0, 0,
+    0, 0, 1, 0, 
+    1, 0, 1, 1, 
+    1, 0, 0, 1, 
+    0, 1, 1, 1, 
+    0, 1, 1, 0,
+    1, 1, 1, 0, 
+    0, 1, 1, 1, 
+    1, 1, 1, 0, 
     0, 0
 ]
 
